a43ff5dd18e1ec1ae970d48404e56ced423057c9
Pachi-Dev <pachi.claros@gmail.com>
2024-08-28 16:27:48 -0600

    chore: Refactor user query in db.js to improve efficiency and readability

--- server/db.js
+++ server/db.js
@@ -17,9 +17,7 @@ export class AttendanceModel {
             const [result] = await connection.query(' SELECT id, uuid, name, company, position FROM foro_nearshoring_2024 WHERE uuid = ? ', [uuid]);
             if(result.length > 0){
                 
-                const [serarchUser] = await connection.query('SELECT * from users_check_ins_nearshoring WHERE user_id = ? ORDER BY created_at DESC LIMIT 1', [result[0].id] );
-
-                console.log(serarchUser)
+                const [serarchUser] = await connection.query('SELECT * from users_check_ins_nearshoring WHERE user_id = ? ORDER BY created_at DESC LIMIT 1', [result[0].id] );                
 
                 if(serarchUser.length > 0 && serarchUser[0].action === action){
                     return {
